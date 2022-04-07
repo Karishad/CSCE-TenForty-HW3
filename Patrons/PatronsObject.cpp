f6972b176e8169f892fc7ace0db8a41cdfaeabee
Kieron Yin <44437832+Karishad@users.noreply.github.com>
2022-04-07 05:13:09 -0500

    Added patrons objects

--- Patrons/PatronsObject.cpp
+++ Patrons/PatronsObject.cpp
@@ -10,3 +10,50 @@
 */
 
 #include "PatronsObject.h"
+
+Patron::Patron()
+{
+    Name = "";
+    ID = 000000;
+    Fines = 0.0;
+    Num_Books_Out = 0;
+};
+Patron::Patron(string name, int id, float fines, int books)
+{
+    Name = name;
+    ID = id;
+    Fines = fines;
+    Num_Books_Out = books;
+};
+void Patron::setName(string name)
+{
+    Name = name;
+}
+string Patron::getName()
+{
+    return Name;
+}
+void Patron::setID(int id)
+{
+    ID = id;
+}
+int Patron::getID()
+{
+    return ID;
+}
+void Patron::setFines(float fines)
+{
+    Fines = fines;
+}
+float Patron::getFines()
+{
+    return Fines;
+}
+void Patron::setBooks(int books)
+{
+    Num_Books_Out = books;
+}
+int Patron::getBooks()
+{
+    return Num_Books_Out;
+}
--- Patrons/PatronsObject.h
+++ Patrons/PatronsObject.h
@@ -1,3 +1,6 @@
+#ifndef PATRONSOBJECT_H
+#define PATRONSOBJECT_H
+
 /*
     CSCE 1040 Homework 3
     Section: 001
@@ -7,5 +10,34 @@
 
     File name: PatronsObject.h
     Description:  Class definition for PatronsObject.cpp
-*/#include <string>
+    Class Parameters
+        string name, int id, float fines, int books
+    Sets/Gets for each Parameter
+*/
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+class Patron
+{
+public:
+    Patron();
+    Patron(string name, int id, float fines, int books);
+    void setName(string name);
+    string getName();
+    void setID(int id);
+    int getID();
+    void setFines(float fines);
+    float getFines();
+    void setBooks(int books);
+    int getBooks();
+
+private:
+    string Name;
+    int ID;
+    float Fines;
+    int Num_Books_Out; // This number cannot exceed 6
+};
+
+#endif PATRONSOBJECT_H
