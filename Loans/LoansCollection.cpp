f099ecd6d56cd1a013f49ef7db94b3a2c1926dc1
Kieron Yin <44437832+Karishad@users.noreply.github.com>
2022-04-22 00:38:37 -0500

    Updated LoansCollection functions

--- Loans/LoansCollection.cpp
+++ Loans/LoansCollection.cpp
@@ -10,3 +10,66 @@
 */
 
 #include "LoansCollection.h"
+
+Loans::Loans()
+{
+    vector<Loan> LoanCollection;
+}
+int Loans::getSize()
+{
+    return LoanCollection.size();
+}
+int Loans::findLoanID(int ID)
+{
+    // Error checking if LoanCollection is empty to prevent errors
+    if (LoanCollection.empty() == true)
+        return -1;
+    for (int i = 0; i < LoanCollection.size(); i++)
+    {
+        if (LoanCollection.at(i).getLoan_ID() == ID)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+Loan Loans::FoundLoanID(int LoanPosition)
+{
+    return LoanCollection.at(LoanPosition);
+}
+void Loans::addLoan(Loan newLoan)
+{
+    LoanCollection.push_back(newLoan);
+}
+void Loans::editLoan(int LoanPosition, Loan newLoan)
+{
+    LoanCollection.at(LoanPosition) = newLoan;
+}
+void Loans::deleteLoan(int LoanPosition)
+{
+    LoanCollection.erase(LoanCollection.begin() + LoanPosition);
+}
+void Loans::printOneLoan(int LoanPosition)
+{
+    cout << "Loan ID: " << LoanCollection.at(LoanPosition).getLoan_ID() << endl;
+    cout << "Book ID: " << LoanCollection.at(LoanPosition).getBook_ID() << endl;
+    cout << "Patron ID: " << LoanCollection.at(LoanPosition).getPatron_ID() << endl;
+    cout << "Due Date: " << LoanCollection.at(LoanPosition).getDueDate() << endl;
+    cout << "Status: " << LoanCollection.at(LoanPosition).getStatus() << endl;
+}
+void Loans::printAllLoans()
+{
+    if (LoanCollection.empty() == true)
+        cout << "There are no Loans registered." << endl;
+    else
+    {
+        for (int i = 0; i < LoanCollection.size(); i++)
+        {
+            cout << "Loan ID: " << LoanCollection.at(i).getLoan_ID() << endl;
+            cout << "Book ID: " << LoanCollection.at(i).getBook_ID() << endl;
+            cout << "Patron ID: " << LoanCollection.at(i).getPatron_ID() << endl;
+            cout << "Due Date: " << LoanCollection.at(i).getDueDate() << endl;
+            cout << "Status: " << LoanCollection.at(i).getStatus() << endl;
+        }
+    }
+}
